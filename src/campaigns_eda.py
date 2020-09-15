434a9d696fa38cc7c5ff41c3bb9b5a33998c92ee
Julia Sokolova <sokolova.js@gmail.com>
2020-09-15 09:29:11 -0700

    cleaned repo

--- src/campaigns_eda.py
+++ src/campaigns_eda.py
@@ -26,7 +26,52 @@ def results_by_campaig_type(campaign_results):
     results_by_type = results_by_type.sort_values(by = 'ROI', ascending = False).reset_index().drop(columns = 'index')
     return results_by_type
 
+def results_by_targeting(campaign_results):
+    '''
+    Parameters
+    ----------
+    campaign_results : pandas DataFrame
+        preprocessed data about campaigns with campaigns_cleanup function 
 
+    Returns
+    -------
+    pandas DataFrame
+        campaigns results grouped by the targeting type
+    '''
+    results_by_targeting = campaign_results.groupby(campaign_results.targeting_type)[
+        'impressions', 
+        'clics', 
+        'spend', 
+        'sales', 
+        'orders', 
+        'units'].sum().reset_index()
+    results_by_targeting['ROI'] = results_by_targeting.sales / results_by_targeting.spend
+    results_by_targeting = results_by_targeting.sort_values(by = 'ROI', ascending = False)[0:2].reset_index().drop(columns = 'index')
+
+    return results_by_targeting
+
+def results_by_daily_budget(campaign_results):
+    '''
+    Parameters
+    ----------
+    campaign_results : pandas DataFrame
+        preprocessed data about campaigns with campaigns_cleanup function 
+
+    Returns
+    -------
+    pandas DataFrame
+        campaigns results grouped by the targeting type
+    '''
+    results_by_daily_budget = campaign_results.groupby(campaign_results.daily_budget)[
+        'impressions', 
+        'clics', 
+        'spend', 
+        'sales', 
+        'orders', 
+        'units'].sum().reset_index()
+    results_by_daily_budget['ROI'] = results_by_daily_budget.sales / results_by_daily_budget.spend
+    results_by_daily_budget = results_by_daily_budget.sort_values(by = 'ROI', ascending = False)
+    return results_by_daily_budget
     
 
 
@@ -37,6 +82,7 @@ if __name__ == '__main__':
 
     # process the data to get campaigns EDA
     campaign_results = campaigns_cleanup(campaign, campaign_details)
-    results_by_type = results_by_campaig_type(campaign_results)
-
+    results_type = results_by_campaig_type(campaign_results)
+    results_targeting = results_by_targeting(campaign_results)
+    results_daily_budget = results_by_daily_budget(campaign_results)
 
