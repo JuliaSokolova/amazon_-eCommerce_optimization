5277ca7bde86a31c52db25acedb4711778b3fe78
Julia Sokolova <sokolova.js@gmail.com>
2020-09-28 15:19:11 -0700

    remove a bag from campaign cleanups

--- src/campaign_data_cleanup.py
+++ src/campaign_data_cleanup.py
@@ -7,9 +7,9 @@ def campaigns_cleanup(campaign, campaign_details):
     '''
     Parameters
     ----------
-    campaign_data : pandas DataFrame
+    campaign: pandas DataFrame
         Contains info about ad campaitgns
-    campaign_details_data : pandas DataFrame
+    campaign_details: pandas DataFrame
         Contains info about ad campaitgns details
 
     Returns
@@ -19,17 +19,18 @@ def campaigns_cleanup(campaign, campaign_details):
     '''
 
     # remove campaigns with zero ads impressions
-    campaign_results = campaign_details.groupby(campaign_details.campaign).sum()
-    campaign_results = campaign_results.reset_index()
-    empty_campaigns = campaign_results[campaign_results.impressions ==0].campaign.values
-    for campaign in empty_campaigns:
-        campaign_results = campaign_results[campaign_results.campaign != campaign]
+    campaign_results = campaign_details.groupby(campaign_details.campaign).sum().reset_index()
+    empty_campaigns = campaign_results[campaign_results.impressions == 0].campaign.values
+    for camp in empty_campaigns:
+        campaign_results = campaign_results[campaign_results.campaign != camp]
     
     # drop columns with unnesessary information
     campaign_results = campaign_results[['campaign', 'impressions', 'clics', 'spend', 'sales', 'orders', 'units']]
+    campaign_results = campaign_results.set_index('campaign')
+    campaign = campaign.set_index('id')
 
     # add info from campaign table
-    campaign_results = campaign_results.join(campaign.set_index('id'), on='campaign')
+    campaign_results = campaign_results.join(campaign)
     campaign_results = campaign_results.drop(columns = ['currency', 'end_date'])
 
     return campaign_results
@@ -51,18 +52,63 @@ def zero_sales_campaigns(campaign_results):
     bad_campaigns = campaign_results[campaign_results.sales == 0].campaign.values
 
     return bad_campaigns
+    
+def placement_cleanup(placement, placement_detail, campaign):
+    '''
+    Parameters
+    ----------
+    placement: pandas DataFrame
+        Contains info about ad placement
+    placement_details: pandas DataFrame
+        Contains info about ad placement details
+    campaign: pandas DataFrame
+        Contains info about ad campaitgns
+
+    Returns
+    -------
+    pandas DataFrame
+        combined data about campaigns, placement and placement details
+        
+    '''
+    placement = placement.rename(columns= {"placement": "placement_type"})
+    placement_detail = placement_detail.join(placement.set_index('id'), on = 'placement')
+    placement_result = placement_detail[[
+        'date', 
+        'impressions', 
+        'clicks', 
+        'spend', 
+        'sales', 
+        'orders', 
+        'units', 
+        'page_views', 
+        'campaign', 
+        'placement_type']]
+    placement_result = placement_result.join(campaign.set_index('id'), on='campaign')
+    placement_result['CpC'] = placement_result.spend / placement_result.clicks
+    placement_result['price'] = placement_result.sales / placement_result.units
+    placement_result['CTR'] = placement_result.clicks / placement_result.impressions * 100
+    placement_result = placement_result.drop(columns = [
+        'name', 
+        'currency', 
+        'attribution_period', 
+        'end_date'])
+
+    return placement_result
 
 
+    
 if __name__ == '__main__':
 
     #import campaign data
     campaign = pd.read_csv('data/AMSCampaign.csv')
     campaign_details = pd.read_csv('data/AMSCampaignDetail.csv')
+    placement = pd.read_csv('data/AMSPlacement.csv')
+    placement_detail = pd.read_csv('data/AMSPlacementDetail.csv')
+
 
     # print campaigns with zero results
     campaign_results = campaigns_cleanup(campaign, campaign_details)
     bad_campaigns = zero_sales_campaigns(campaign_results)
     for camp in bad_campaigns:
         print(campaign_details[campaign_details.campaign == camp].sum())
-            
-    
+    placement_result = placement_cleanup(placement, placement_detail, campaign)
--- src/campaigns_eda.py
+++ src/campaigns_eda.py
@@ -1,4 +1,5 @@
-from campaign_data_cleanup import campaigns_cleanup
+#from campaign_data_cleanup import campaigns_cleanup, placement_cleanup
+
 import pandas as pd
 import numpy as np
 
@@ -71,18 +72,50 @@ def results_by_daily_budget(campaign_results):
         'units'].sum().reset_index()
     results_by_daily_budget['ROI'] = results_by_daily_budget.sales / results_by_daily_budget.spend
     results_by_daily_budget = results_by_daily_budget.sort_values(by = 'ROI', ascending = False)
-    return results_by_daily_budget
     
+    return results_by_daily_budget
+
+def result_by_placement(placement_result):
+    '''
+    Parameters
+    ----------
+    placement_result : pandas DataFrame
+        preprocessed data about ads placement with placement_result function 
+
+    Returns
+    -------
+    pandas DataFrame
+        campaigns results grouped by the placement type   
+    '''
+    placements_campaign_type = placement_result.groupby(
+        [
+        placement_result.type, 
+        placement_result.placement_type
+        ]
+        )['impressions', 
+            'clicks', 
+            'spend', 
+            'sales', 
+            'orders', 
+            'units'].sum().reset_index()
+    placements_campaign_type['ROI'] = placements_campaign_type.sales / placements_campaign_type.spend
+    placements_campaign_type = placements_campaign_type.sort_values(by = 'ROI', ascending = False)
+
+    return placements_campaign_type
 
 
 if __name__ == '__main__':
     #import campaign data
     campaign = pd.read_csv('data/AMSCampaign.csv')
     campaign_details = pd.read_csv('data/AMSCampaignDetail.csv')
+    placement = pd.read_csv('data/AMSPlacement.csv')
+    placement_detail = pd.read_csv('data/AMSPlacementDetail.csv')
 
     # process the data to get campaigns EDA
     campaign_results = campaigns_cleanup(campaign, campaign_details)
+    placement_result = placement_cleanup(placement, placement_detail, campaign)
     results_type = results_by_campaig_type(campaign_results)
     results_targeting = results_by_targeting(campaign_results)
     results_daily_budget = results_by_daily_budget(campaign_results)
+    result_by_placement = result_by_placement(placement_result)
 
